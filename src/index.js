93d32dfcaa278cc9e501659a5f8e785b8b99640e
Talita-Izabel <talitazbl@gmail.com>
2021-04-28 17:08:56 -0300

    api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
 app.locals.layouts = false;
 //Para que ele entenda quando enviar um requisição em json para API com informações em json.
-app.use(bodyParser.json());
+app.use(express.json());
 //Para entender quando passar parâmetros via URL.
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false})); 
 
 //Referenciando os controllers e repassar o app.
 require('./app/controllers/index')(app);
